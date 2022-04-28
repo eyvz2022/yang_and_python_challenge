b4a4f78b708ee5d8d6ebd47ef56d1535e4bae034
eyvz2022 <eyavuz21@student.aau.dk>
2022-04-28 06:34:42 +0200

    commit on main

--- TASK2/SOLUTION1/input.py
+++ TASK2/SOLUTION1/input.py
@@ -1,6 +1,7 @@
+#Import Config class from config.py
 from config import Config
 
-#Interface List: Input is taken from here
+#Interface List: Input is taken here
 interfaces = [{
     "intf-type": "GigabitEthernet",
     "intf-id": "0/0",
@@ -27,8 +28,11 @@ interfaces = [{
     }
 ]
 
+#Call config.py script
 cfg = Config()
 output = ''
+
+#This for loop sends all interface data to jinja over config.py and have the proper results
 for interface in interfaces:
     output += cfg.create_interface_config(interface_info_dict=interface)
 print(output)
