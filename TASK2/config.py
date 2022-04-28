aca752262a6491478fcbc2be2bbd6ca2eeaa07d0
eyvz2022 <103566085+eyvz2022@users.noreply.github.com>
2022-04-28 05:30:58 +0200

    Create config.py

---
+++ TASK2/config.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+class Config:
+
+    def __init__(self):
+        self.file_loader = FileSystemLoader("./jinja")
+        self.env = Environment(loader=self.file_loader, trim_blocks=True, lstrip_blocks=True)
+
+    def create_interface_config(self, interface_info_dict={}):
+        template = self.env.get_template("interface.j2")
+        output = template.render(dict_item=interface_info_dict)
+
+        return output
